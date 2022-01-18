9174898026fb0cbdb1339c10566efdaf3eb56cb8
sth4881 <sth4881@naver.com>
2022-01-18 17:39:20 +0900

    [Day1] 🔥 자바 프로그래밍 실습코드

---
+++ src/com/kosa/ch1/MyFirstApp.java
@@ -0,0 +1,7 @@
+package com.kosa.ch1;
+
+public class MyFirstApp {
+	public static void main(String[] args) {
+		System.out.println("I rule!");
+	}
+}
---
+++ src/com/kosa/ch1/WhileDemo.java
@@ -0,0 +1,11 @@
+package com.kosa.ch1;
+
+public class WhileDemo {
+    public static void main(String[] args){
+        int count = 1;
+        while (count < 11) {
+            System.out.println("Count is: " + count);
+            count++;
+        }
+    }
+}
